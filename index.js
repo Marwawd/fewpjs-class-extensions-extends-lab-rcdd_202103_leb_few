a50e3acd6bc053ddee6d0c76dde00be23ad52ee4
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-08 22:10:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Triangle extends Polygon {
     else if(this.array[0]+this.array[2]>this.array[1]) return true;
     else if(this.array[1]+this.array[2]>this.array[0]) return true;
     else return false ;
-  
+
   }
 }
 
