bd39940b62d4631d38430f94fb568b4fef33a573
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-08 22:18:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,11 +28,10 @@ class Square extends Polygon {
   get area(){
     return this.array[0]*this.array[0]
   }
-get isValid(){
+  get isValid(){
   return ((this.array[0]===this.array[1]) &&
     (this.array[1]===this.array[2]) &&
     (this.array[2]===this.array[3])
-
 )
-}
+  }
 }
