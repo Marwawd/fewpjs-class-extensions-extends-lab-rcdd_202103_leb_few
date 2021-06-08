8ce90a38a8bec898b371573f4ce47cc71d91725b
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-08 21:57:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+class Polygon{
+  constructor(array) {
+      this.array = array;
+    }
+    get countSides(){
+      return this.array.length;
+    }
+    get perimeter(){
+      let sum=0;
+      let countsides=this.countSides;
+      for(let i=0 ; i<=countsides;i++)
+      {
+      sum +=countsides[i]
+      }
+      return sum;
+
+    }
+}
+class Triangle extends Polygon {
+  // inherits constructor
+}
+
+class Square extends Polygon {
+  area(){
+    return this.array[0]*this.array[0]
+  }
+
+}
